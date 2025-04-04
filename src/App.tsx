ca14b1da42178207f17fd6fbdf101bf8fafcbaff
Hoon <hoon0661@gmail.com>
2025-04-04 19:28:54 -0400

    added basic setup in the root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 function App() {
     return (
-        <main>
-            <div>hello</div>
+        <main
+            className={"relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"}>
+            <div className={"max-w-7xl w-full"}>
+                
+            </div>
         </main>
     )
 }
