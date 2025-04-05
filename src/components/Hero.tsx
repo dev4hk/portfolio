ea89171c53194e99ddd6224086f205241f11f3ee
Hoon <hoon0661@gmail.com>
2025-04-05 12:02:32 -0400

    added css to span hero entire screen height

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,8 @@ import {heroFlipWords} from "../data/data.ts";
 
 const Hero = () => {
     return (
-        <section className={"w-screen"}>
-            <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+        <section className={"w-screen h-screen"}>
+            <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 h-full">
                 <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-3xl lg:text-5xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
                     "Hi, I build sleek, performant <FlipWords words={heroFlipWords}/>experiences."
                 </h2>
