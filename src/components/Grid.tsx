5e18c5cf1ecebb5c66a835f122ce7bc058420247
Hoon <hoon0661@gmail.com>
2025-06-25 12:48:56 -0400

    replaced github and demo buttons with icons

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -25,17 +25,18 @@ const Grid = () => {
                     <AnimatedTooltip items={items}/>
                 </div>
                 <div className={"flex justify-end gap-3"}>
-                    {project.demo !== undefined && (
+                    {project.demo && (
                         <a href={project.demo} target="_blank" rel="noopener noreferrer">
                             <button
-                                className="relative inline-flex h-12 overflow-hidden rounded-3xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                                className="relative inline-flex overflow-hidden rounded-3xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
                             >
                                 <span
                                     className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"/>
-                                <span
-                                    className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-3xl bg-slate-950 px-4 py-2 text-sm font-medium text-white backdrop-blur-3xl">
-        Demo
-      </span>
+                                <div
+                                    className="p-1 bg-black rounded-3xl relative group transition duration-200 text-white hover:bg-transparent flex items-center justify-center">
+                                    <img src="./assets/logos/Rocket.svg" alt="Details"
+                                         className="w-6 h-6 brightness-0 invert"/>
+                                </div>
                             </button>
                         </a>
                     )}
@@ -44,13 +45,13 @@ const Grid = () => {
                         href={project.link}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="p-[3px] relative block"
+                        className="p-[2px] relative block"
                     >
 
                         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-3xl"/>
                         <div
-                            className="px-4 py-2  bg-black rounded-3xl  relative group transition duration-200 text-white hover:bg-transparent">
-                            Details
+                            className="px-1 py-1 bg-black rounded-3xl relative group transition duration-200 text-white hover:bg-transparent flex items-center justify-center">
+                            <img src="./assets/logos/Github.svg" alt="Details" className="w-6 h-6 brightness-0 invert"/>
                         </div>
                     </a>
                 </div>
